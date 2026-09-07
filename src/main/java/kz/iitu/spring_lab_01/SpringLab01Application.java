7b0101a1abcf2a7d0e68845be405a896139fb177
Sabyr Daryn <40524@iitu.edu.kz>
2026-09-07 16:42:02 +0500

    lab01: generated Spring Boot project skeleton

---
+++ src/main/java/kz/iitu/spring_lab_01/SpringLab01Application.java
@@ -0,0 +1,13 @@
+package kz.iitu.spring_lab_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLab01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLab01Application.class, args);
+	}
+
+}
